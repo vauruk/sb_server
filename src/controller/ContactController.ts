546c684500d8ad77baf1db6ce9505ec666d73f71
Vanderson Vauruk <vauruk@gmail.com>
2021-10-27 11:56:11 -0300

    ajuste de pesquisa no servidor

--- src/controller/ContactController.ts
+++ src/controller/ContactController.ts
@@ -3,19 +3,25 @@ import { Request, Response } from 'express'
 import contactList from '../data/contacts'
 import { ContactProps } from './ContactType'
 
+import { age } from '../util/dateUtil'
+
 class ContactContoller {
+
+    private find = (text: string) => {
+        console.log(text)
+        return null
+    }
+
     public async index(req: Request, res: Response): Promise<Response> {
-        //console.log("req xxß", req.params)
         const search = req.params.search
-        console.log("filter", search)
-        let newList: ContactProps[] = contactList.filter(item => item.name.includes(search))
+        const text = search.split(' ')
+        console.log("text", text)
+        let newList: ContactProps[] = contactList.filter(item =>
+            item.name.includes(search)
+        )
+
         return res.json(newList);
     }
 
-    private find(value: string) {
-        console.log("filter", value)
-        let newList: ContactProps[] = contactList.filter(item => item.name.includes(value))
-        return newList;
-    }
 }
 export default new ContactContoller();
